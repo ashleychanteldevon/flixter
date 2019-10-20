03a9e86d2ec3e43c1b40b0db3d304bb6cabea544
Ashley Devon <ashleychanteldevon@gmail.com>
2019-10-20 02:01:11 +0000

    adding stripe and payment form

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,20 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_to_view_lessons, only: [:show]
+  
   def show
   end
-
+  
   private
-
+  
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+      
+    def require_enrollment_to_view_lessons
+      if current_user.enrolled_in?(current_lesson.section.course) == false
+        redirect_to course_path(current_lesson.section.course), alert: 'NOT CURRENTLY ENROLLED'
+    end
   end
 end
