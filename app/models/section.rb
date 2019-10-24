9d53a7143d4266c12fd505380331dda7a43a0f53
Ashley Devon <ashleychanteldevon@gmail.com>
2019-10-24 17:53:43 +0000

    adjusting model and migration file for sections

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,14 +3,6 @@ class Section < ApplicationRecord
   has_many :lessons
 
   include RankedModel
-  ranks :row_order, with_same: :section_id
-
-  #def next_section
-    #section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
-    #if lesson.blank? && section.next_section
-      #return section.next_section.lessons.rank(:row_order).first
-    #end
-    #return section
-  #end
+  ranks :row_order, with_same: :course_id
 
 end
